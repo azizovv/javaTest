295e7a3dcb324e2986e60d2120c8b64b5d48840b
D15125709Abdulaziz <34353485+azizovv@users.noreply.github.com>
2018-02-14 17:12:04 +0000

    Add files via upload

---
+++ Tune.java
@@ -0,0 +1,32 @@
+package ie.dit;
+
+	public class Tune
+	{
+		private int x;
+		private String title;
+		private String altTitle;
+		private String title;
+		private String notation;
+	}
+	
+	 //Constructor method
+   public Tune(int x,String title, String altTitle, String notation)
+   {
+     this.title = title;
+     this.altTitle = altTitle;
+     this.notation = notation;
+     this.address = address;
+	 this.x =x;
+   }
+	 public String toString() {
+        return "3, Banish Misfortune.:" + this.x + ",, "
+                + "4, piper's Chair,.:" + this.title + ",, " 
+                + this.altTitle + ",, ";
+    }
+  
+ }
+		
+		
+		
+		
+		
